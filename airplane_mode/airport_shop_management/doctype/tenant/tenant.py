3c16220607db73533843887ee89d12f33639c60f
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2024-08-02 17:28:02 +0530

    DT Created: Maintenance Log , Shop Rent, Rent Payment Entry, Shop Management Settings, Shop ,Shop Contract & Tenant

---
+++ airplane_mode/airport_shop_management/doctype/tenant/tenant.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024, anwarpatelnoori and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class Tenant(Document):
+	def before_validate(self):
+		full_name_and_email(self)
+
+
+def full_name_and_email(doc):
+	if doc.last_name: 	
+		doc.full_name = f"{doc.first_name} {doc.last_name}"
+	else:
+		doc.full_name = doc.first_name
+	doc.email = f"anwar+{doc.first_name.lower()}@standardtouch.com"
