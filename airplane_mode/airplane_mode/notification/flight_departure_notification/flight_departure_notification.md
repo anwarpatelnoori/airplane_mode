44542493bb3828aba10b86423072c71dc5873b2a
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2024-08-05 02:15:53 +0530

    added email reminder and contract expire script

--- airplane_mode/airplane_mode/notification/flight_departure_notification/flight_departure_notification.md
+++ airplane_mode/airplane_mode/notification/flight_departure_notification/flight_departure_notification.md
@@ -1,2 +1,3 @@
 <p>Dear System Manager,</p>
+
 <p>The flight <strong>{{ doc.name }}</strong> is scheduled to depart in 24 hours.</p>
--- airplane_mode/airport_shop_management/doctype/cron_job.py
+++ airplane_mode/airport_shop_management/doctype/cron_job.py
@@ -1,2 +1,16 @@
 import frappe
 
+all_ongoing_contracts = frappe.get_list("Shop Contract", fields=["name", "test_contract_start","test_contract_end"], filters = {"status":"Ongoing"})
+current_time = frappe.utils.now_datetime()
+for contract in all_ongoing_contracts:
+    contract_name = contract['name']
+    test_contract_start = contract['test_contract_start']
+    test_contract_end = contract['test_contract_end']
+    if(test_contract_start<=current_time<=test_contract_end):
+        shop_rent_doc = frappe.new_doc('Monthly Shop Rent')
+        shop_rent_doc.shop_contract = contract_name
+        shop_rent_doc.insert(ignore_permissions = True)
+        shop_rent_doc.submit()
+        frappe.db.commit()
+    if(current_time>test_contract_end):
+        frappe.db.set_value('Shop Contract',contract_name,'status','Expired')
