d7f326e7c32517186bbd286f87a3275c339d9072
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2024-08-30 02:06:03 +0530

    task complete:)

--- airplane_mode/airport_shop_management/doctype/maintenance_log/test_maintenance_log.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, anwarpatelnoori and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestMaintenanceLog(FrappeTestCase):
-	pass
