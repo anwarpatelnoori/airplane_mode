9a00d9376ba2d9f3ee926d4ed37912ab3fe7cb09
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2024-08-29 23:29:14 +0530

    added gate_number functionaility

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
@@ -1,8 +1,62 @@
 // Copyright (c) 2024, anwarpatelnoori and contributors
 // For license information, please see license.txt
+frappe.ui.form.on("Airplane Flight", {
+    refresh(frm) {
+        change_gate_number(frm);
+    },
+});
 
-// frappe.ui.form.on("Airplane Flight", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function change_gate_number(frm) {
+    if (frm.doc.docstatus == 1) {
+        frm.add_custom_button('Change Gate Number', function() {
+            frappe.prompt(
+                [
+                    {
+                        fieldname: 'gate',
+                        fieldtype: 'Link',
+                        options: 'Airport Gate',
+                        label: 'New Gate Number',
+                        reqd: 1
+                    }
+                ],
+                function(values) {
+                    console.log(values);
+                    frappe.call({
+                        method: 'frappe.client.get_list',
+                        args: {
+                            doctype: 'Airplane Ticket',
+                            filters: {
+                                'flight': frm.doc.name
+                            },
+                            fields: ['name', 'gate_number'] // Specify the fields you want to retrieve
+                        },
+                        callback: function(response) {
+                            if (response.message) {
+                                let tickets = response.message;
+                                tickets.forEach(function(ticket) {
+                                    frm.set_value('gate_number',values.gate),
+                                    frappe.call({
+                                        method: 'frappe.client.set_value',
+                                        args: {
+                                            doctype: 'Airplane Ticket',
+                                            name: ticket.name,  // Set the ticket's name
+                                            fieldname: 'gate_number',
+                                            value: values.gate   // Set the new gate number
+                                        },
+                                        callback: function(response) {
+                                            if (!response.exc) {
+                                                frappe.msgprint(`Ticket ${ticket.name} updated with gate number ${values.gate}`);
+                                            }
+                                        }
+                                    });
+                                });
+                            }
+                        }
+                    });
+                },
+                'Change Gate Number',
+                'Change'
+            );
+        });
+    }
+}
--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -6,41 +6,25 @@ from frappe.website.website_generator import WebsiteGenerator
 from frappe.model.document import Document
 from frappe.website.website_generator import WebsiteGenerator
 class AirplaneFlight(WebsiteGenerator):
-	def before_submit(self):
-		# print 20 new lines
-		print("\n"*20)
-		print("Before Submit")
-		self.status = "Completed"
-	def before_save(self):
+	def before_validate(self):
 
 		# Need Crew Members
 		if len(self.flight_crew_members)<=0:
 			frappe.throw('Please Add Flight Crew Members')
-		# Not more than 5 Crew Members
-		if len(self.flight_crew_members)>=5:
-			frappe.throw('Flight Crew Members cannot be more than 5')
 
 		# Atleast 1 Pilot and 1 Flight Attendants
+		pilot_counter = 0
+		attendants_counter = 0
 		for i in self.flight_crew_members:
 			if i.role == 'Pilot':
-				break
-			else:
-				frappe.throw('Please Add a Pilot')
-		for j in self.flight_crew_members:
-			if j.role == 'Flight Attendants':
-				break
-			else:
-				frappe.throw('Please Add Flight Attendants')
-		# Maximum 2 Pilots and 3 Flight Att
-		for k in self.flight_crew_members:
-			pilot_counter = 0
-			if k.role == 'Pilot':
 				pilot_counter+=1
-				if pilot_counter>=3:
-					frappe.throw('Please Add Below 3 Pilot')
-		for l in self.flight_crew_members:
-			attendants_counter = 0
-			if l.role == 'Flight Attendants':
-				attendants_counter+=1
-				if attendants_counter>=4:
-					frappe.throw('Please Add Below 4 Flight Attendants')
+			elif i.role =='Flight Attendants':
+				attendants_counter +=1
+		if pilot_counter<=0:
+			frappe.throw('My Airplane is not Fully Automated 🙃 Add one Pilot')
+		elif pilot_counter>=3:
+			frappe.throw('No More Pilots 😇, Only 2 Allowed, remove extra')
+		elif attendants_counter<=0:
+			frappe.throw('Add 1 Flight Attendant,You Need Support in Sky 😉')
+		elif attendants_counter>=4:
+			frappe.throw('No More Flight Attendants 😇,Only 3 required, remove Extra')
